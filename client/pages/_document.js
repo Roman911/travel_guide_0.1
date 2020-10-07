64cda2da17df79dae8d4c895865ba4e17afb12b0
Roman911 <roma-lysyk@ukr.net>
2020-10-07 08:37:45 +0300

    initial client

---
+++ client/pages/_document.js
@@ -0,0 +1,35 @@
+import Document, {Head, Main, NextScript} from "next/document"
+import React from "react"
+import {StyleSheetServer} from "aphrodite"
+
+export default class MyDocument extends Document {
+  static async getInitialProps({renderPage}) {
+    const {html, css} = StyleSheetServer.renderStatic(() => renderPage())
+    const ids = css.renderedClassNames
+    return {...html, css, ids}
+  }
+
+  constructor(props) {
+    super(props)
+    const {__NEXT_DATA__, ids} = props
+    if (ids) {
+      __NEXT_DATA__.ids = this.props.ids
+    }
+  }
+
+  render() {
+    return (
+      <html>
+      <Head>
+        <title>Travel Guide</title>
+        <link rel="icon" href="/favicon.ico"/>
+        <style data-aphrodite dangerouslySetInnerHTML={{__html: this.props.css.content}}/>
+      </Head>
+      <body>
+      <Main/>
+      <NextScript/>
+      </body>
+      </html>
+    )
+  }
+}
